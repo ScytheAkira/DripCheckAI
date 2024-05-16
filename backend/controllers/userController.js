ba662f487c4d2a7a8e447524a6d0a31bcf399e03
Ikram Ul Haq <ihaq1133@gmail.com>
2024-05-17 00:37:23 +0500

    feat: backend routing and controls ; admin and user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,7 +24,6 @@ const createUser = asyncHandler(async(req, res)=>
 
 
     const salt = await bcrypt.genSalt(10);
-
     const hashedPassword = await bcrypt.hash(password, salt);
 
 
@@ -104,9 +103,12 @@ const updateCurrentUserProfile = asyncHandler(async(req, res) => {
         user.email = req.body.email || user.email;
 
         if(req.body.password){
-            user.password = req.body.password;
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(req.body.password, salt);
+            user.password = hashedPassword;
+
         }
-        const updatedUser = await User.save();
+        const updatedUser = await user.save();
         res.json({
             _id: updatedUser._id,
             username: updatedUser.username,
@@ -118,4 +120,66 @@ const updateCurrentUserProfile = asyncHandler(async(req, res) => {
         throw new Error("User not Found");
     }
 })
-export{createUser, loginUser, logoutCurrentUser, getAllUsers, getCurrentUserProfile, updateCurrentUserProfile};
+
+
+const deleteUserById = asyncHandler(async(req, res) =>{
+    const user = await User.findById(req.params.id);
+    if(user){
+        if(user.isAdmin){
+            res.status(400);
+            throw new Error('Cannot delete Admin User');
+
+        }
+        await User.deleteOne({_id: user._id});
+        res.json({message: "User Removed"});
+
+    }else{
+        res.status(404);
+        throw new Error("User not found");
+    }
+})
+
+const getUserById = asyncHandler(async(req,res)=> {
+    const user = await User.findById(req.params.id).select('-password');
+    
+    if(user){
+        res.json(user);
+    }else{
+        res.status(404);
+        throw new Error('User not found');
+    }
+})
+
+const updateUserById = asyncHandler(async(req, res) =>{
+    const user = await User.findById(req.params.id)
+
+    if(user){
+        user.username = req.body.username || user.username;
+        user.email = req.body.email || user.email;
+        user.isAdmin = Boolean(req.body.isAdmin);
+
+        const updatedUser = await user.save();
+
+        res.json({
+            _id: updatedUser._id,
+            username: updatedUser.username,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin
+        })
+
+    }else{
+        res.status(404);
+        throw new Error('User not found');
+    }
+})
+export{
+    createUser, 
+    loginUser, 
+    logoutCurrentUser, 
+    getAllUsers, 
+    getCurrentUserProfile, 
+    updateCurrentUserProfile,
+    deleteUserById,
+    getUserById,
+    updateUserById
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,20 +4,34 @@ import { createUser,
         logoutCurrentUser,
         getAllUsers,
         getCurrentUserProfile,
-        updateCurrentUserProfile
+        updateCurrentUserProfile,
+        deleteUserById,
+        getUserById,
+        updateUserById
     } from "../controllers/userController.js";
 
 import {authenticate, authorizeAdmin} from '../middlewares/authMiddleware.js'
 
 const router = express.Router();
 
-router.route('/').post(createUser).get(authenticate, authorizeAdmin, getAllUsers);
+router
+    .route('/')
+    .post(createUser)
+    .get(authenticate, authorizeAdmin, getAllUsers);
 
 //router.route('/').post(createUser) is the same as router.post('/', createUser)
 router.post('/auth', loginUser) ;
 router.post('/logout', logoutCurrentUser);
 
-router.route('/profile').get(authenticate, getCurrentUserProfile).put(authenticate, updateCurrentUserProfile);
+router
+    .route('/profile')
+    .get(authenticate, getCurrentUserProfile)
+    .put(authenticate, updateCurrentUserProfile);
 
-
+//admin routes 👇
+router
+    .route('/:id')
+    .delete(authenticate, authorizeAdmin, deleteUserById)
+    .get(authenticate, authorizeAdmin, getUserById)
+    .put(authenticate, authorizeAdmin, updateUserById);
 export default router;
